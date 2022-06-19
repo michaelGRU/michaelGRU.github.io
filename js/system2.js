a0b023a94c500d3c581f1fac6522fdd89190ee6e
SMW <michaelwang502@yahoo.com>
2022-06-18 20:29:13 -0700

    add control button to control network

--- js/system2.js
+++ js/system2.js
@@ -13,7 +13,7 @@ function drawGraph() {
     var big = 25;
 
     // parsing and collecting nodes and edges from the python
-    nodes = new vis.DataSet([{ "color": "rgba(255,0,0, 0.95)", "font": { "color": "white" }, "id": "My Skills", "label": "", "image": "../img/robot.png", "shape": "image", "size": 66, 'url': 'https://michaelgru.github.io/' }, { "color": "rgb(255, 241, 118)", "font": {size:big, "color": "white" }, "id": "Social Sciences", "label": "Social Sciences", "shape": "star" }, { "color": "#78CF8A", "font": {size:big, "color": "white" }, "id": "Mathematics", "label": "Mathematics", "shape": "star" }, { "color": "#A6E22E", "font": {size:big, "color": "white" }, "id": "Computer Science", "label": "Computer Science", "shape": "star" }, { "color": "#A6E22E", "font": {"color": "white" }, "id": "Python", "image": "icon/python.png", "label": "Python", "shape": "image", "size": 10 }, { "color": "#A6E22E", "font": { "color": "white" }, "id": "R", "image": "icon/R.png", "label": "R", "shape": "image", "size": 10 }, { "color": "#A6E22E", "font": { "color": "white" }, "id": "SQL", "image": "icon/db.png", "label": "SQL", "shape": "image", "size": 10 }, { "color": "#A6E22E", "font": { "color": "white" }, "id": "Machine Learning", "label": "Machine Learning", "shape": "square", "size": 10 }, { "color": "#A6E22E", "font": { "color": "white" }, "id": "Bash & Linux Systems", "label": "Bash & Linux Systems", "shape": "square", "size": 10 }, { "color": "#A6E22E", "font": { "color": "white" }, "id": "Data Structures", "label": "Data Structures", "shape": "square", "size": 10 }, { "color": "#A6E22E", "font": { "color": "white" }, "id": "Web Design", "label": "Web Design", "shape": "square", "size": 10 }, { "color": "#A6E22E", "font": { "color": "white" }, "id": "Database Design", "label": "Database Design", "shape": "square", "size": 10 },
+    nodes = new vis.DataSet([{ "color": "rgba(255,0,0, 0.95)", "font": { "color": "white" }, "id": "My Skills", "label": "", "image": "../img/robot.png", "shape": "image", "size": 66, 'url': 'https://michaelgru.github.io/' }, { "color": "rgb(255, 241, 118)", "font": { size: big, "color": "white" }, "id": "Social Sciences", "label": "Social Sciences", "shape": "star" }, { "color": "#78CF8A", "font": { size: big, "color": "white" }, "id": "Mathematics", "label": "Mathematics", "shape": "star" }, { "color": "#A6E22E", "font": { size: big, "color": "white" }, "id": "Computer Science", "label": "Computer Science", "shape": "star" }, { "color": "#A6E22E", "font": { "color": "white" }, "id": "Python", "image": "icon/python.png", "label": "Python", "shape": "image", "size": 10 }, { "color": "#A6E22E", "font": { "color": "white" }, "id": "R", "image": "icon/R.png", "label": "R", "shape": "image", "size": 10 }, { "color": "#A6E22E", "font": { "color": "white" }, "id": "SQL", "image": "icon/db.png", "label": "SQL", "shape": "image", "size": 10 }, { "color": "#A6E22E", "font": { "color": "white" }, "id": "Machine Learning", "label": "Machine Learning", "shape": "square", "size": 10 }, { "color": "#A6E22E", "font": { "color": "white" }, "id": "Bash & Linux Systems", "label": "Bash & Linux Systems", "shape": "square", "size": 10 }, { "color": "#A6E22E", "font": { "color": "white" }, "id": "Data Structures", "label": "Data Structures", "shape": "square", "size": 10 }, { "color": "#A6E22E", "font": { "color": "white" }, "id": "Web Design", "label": "Web Design", "shape": "square", "size": 10 }, { "color": "#A6E22E", "font": { "color": "white" }, "id": "Database Design", "label": "Database Design", "shape": "square", "size": 10 },
     { "color": "rgb(255, 241, 118)", "font": { "color": "white" }, "id": "Social Network Analysis", "label": "Social Network Analysis", "shape": "diamond", "size": 10 }, { "color": "rgb(255, 241, 118)", "font": { "color": "white" }, "id": "Social Data Ethics", "label": "Social Data Ethics", "shape": "diamond", "size": 10 }, { "color": "rgb(255, 241, 118)", "font": { "color": "white" }, "id": "Social Media Mining", "label": "Social Media Mining", "shape": "diamond", "size": 10 }, { "color": "rgb(255, 241, 118)", "font": { "color": "white" }, "id": "Network Visualization", "label": "Network Visualization", "shape": "diamond", "size": 10 }, { "color": "rgb(255, 241, 118)", "font": { "color": "white" }, "id": "Network Modeling", "label": "Network Modeling", "shape": "diamond", "size": 10 }, { "color": "#78CF8A", "font": { "color": "white" }, "id": "Minor in Mathematics", "label": "Minor in Mathematics", "shape": "dot", "size": 10 }, { "color": "#78CF8A", "font": { "color": "white" }, "id": "B.Sc. Physics", "label": "B.Sc. Physics", "shape": "dot", "size": 10 }, { "color": "#78CF8A", "font": { "color": "white" }, "id": "M.Sc. Info. Mgmt Data Science ", "label": "M.Sc. Info. Mgmt Data Science ", "shape": "dot", "size": 10 }]);
     edges = new vis.DataSet([{ "from": "My Skills", "to": "Computer Science", "weight": 2 }, { "from": "My Skills", "to": "Mathematics", "weight": 2 }, { "from": "My Skills", "to": "Social Sciences", "weight": 2 }, { "from": "Computer Science", "to": "Machine Learning", "weight": 1 }, { "from": "Computer Science", "to": "Bash & Linux Systems", "weight": 1 }, { "from": "Computer Science", "to": "Data Structures", "weight": 1 }, { "from": "Machine Learning", "to": "Python", "weight": 1 }, { "from": "Machine Learning", "to": "R", "weight": 1 }, { "from": "Machine Learning", "to": "SQL", "weight": 1 }, { "from": "Mathematics", "to": "B.Sc. Physics", "weight": 1 }, { "from": "Mathematics", "to": "M.Sc. Info. Mgmt Data Science ", "weight": 1 }, { "from": "Mathematics", "to": "Minor in Mathematics", "weight": 1 }, { "from": "Social Sciences", "to": "Social Data Ethics", "weight": 1 }, { "from": "Social Sciences", "to": "Social Network Analysis", "weight": 1 }, { "from": "Social Sciences", "to": "Social Media Mining", "weight": 1 }, { "from": "Social Network Analysis", "to": "Network Modeling", "weight": 1 }, { "from": "Social Network Analysis", "to": "Network Visualization", "weight": 1 }, { "from": "Computer Science", "to": "Web Design", "weight": 1 }, { "from": "Web Design", "to": "Database Design", "weight": 1 }]);
 
@@ -46,7 +46,8 @@ function drawGraph() {
             "hover": true,
             "dragNodes": true,
             "hideEdgesOnDrag": false,
-            "hideNodesOnDrag": false
+            "hideNodesOnDrag": false,
+            "navigationButtons": true,
         },
         "physics": {
             "enabled": true,
@@ -60,7 +61,7 @@ function drawGraph() {
             },
             "solver": "forceAtlas2Based",
             "stabilization": {
-                "enabled": true,
+                "enabled": false,
                 "fit": true,
                 "iterations": 10000,
                 "onlyDynamicEdges": false,
@@ -153,5 +154,40 @@ elementToFindDigitsIn.innerHTML =
     elementToFindDigitsIn
         .textContent
         .replace(/(\$?\d+)/g, '<span>$1</span>');
+
+
+
+//Get the canvas HTML element
+var networkCanvas = document.getElementById("mynetwork").getElementsByTagName("canvas")[0]
+function changeCursor(newCursorStyle) {
+    networkCanvas.style.cursor = newCursorStyle;
+}
+function changeEventCursor(eventName, cursorType) {
+    network.on(eventName, function () {
+        changeCursor(cursorType);
+    });
+}
+network.on('hoverNode', function () {
+    changeCursor('pointer');
+});
+network.on('blurNode', function () {
+    changeCursor('default');
+});
+network.on('hoverEdge', function () {
+    changeCursor('grab');
+});
+network.on('blurEdge', function () {
+    changeCursor('default');
+});
+network.on('dragStart', function () {
+    changeCursor('grabbing');
+});
+network.on('dragging', function () {
+    changeCursor('grabbing');
+});
+network.on('dragEnd', function () {
+    changeCursor('grab');
+});
+
 
 
