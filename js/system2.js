cd78509f660f793256e26e00a8aa312ad718744e
SMW <michaelwang502@yahoo.com>
2022-06-19 17:26:13 -0700

    add edge animation

--- js/system2.js
+++ js/system2.js
@@ -19,67 +19,68 @@ function drawGraph() {
     var big = 25;
 
     // parsing and collecting nodes and edges from the python
-    nodes = new vis.DataSet([{"font": { "color": "white" }, "id": "My Skills", "label": "", "image": "../img/robot.png", "shape": "image", "size": 66, 'url': 'https://michaelgru.github.io/', group: 0},
+    nodes = new vis.DataSet([{ "font": { "color": "white" }, "id": "My Skills", "label": "", "image": "../img/robot.png", "shape": "image", "size": 66, 'url': 'https://michaelgru.github.io/', group: 0 },
 
-    {"font": { size: big, "color": "white", strokeWidth: 1, strokeColor: 'white'}, "id": "Social Sciences", "label": "Social Sciences", "shape": "star", group: 1},
+    { "font": { size: big, "color": "white", strokeWidth: 1, strokeColor: 'white' }, "id": "Social Sciences", "label": "Social Sciences", "shape": "star", group: 1 },
 
-    {"font": { size: big, "color": "white", strokeWidth: 1, strokeColor: 'white'}, "id": "Mathematics", "label": "Mathematics", "shape": "star", group: 2 },
+    { "font": { size: big, "color": "white", strokeWidth: 1, strokeColor: 'white' }, "id": "Mathematics", "label": "Mathematics", "shape": "star", group: 2 },
 
-    {"font": { size: big, "color": "white", strokeWidth: 1, strokeColor: 'white'}, "id": "Computer Science", "label": "Computer Science", "shape": "star", group: 3 },
+    { "font": { size: big, "color": "white", strokeWidth: 1, strokeColor: 'white' }, "id": "Computer Science", "label": "Computer Science", "shape": "star", group: 3 },
 
-    {"font": { "color": "white" }, "id": "Python", "image": "icon/python.png", "label": "Python", "shape": "image", "size": 10, group: 3 },
+    { "font": { "color": "white" }, "id": "Python", "image": "icon/python.png", "label": "Python", "shape": "image", "size": 10, group: 3 },
 
-    {"font": { "color": "white" }, "id": "R", "image": "icon/R.png", "label": "R", "shape": "image", "size": 10, group: 3 },
+    { "font": { "color": "white" }, "id": "R", "image": "icon/R.png", "label": "R", "shape": "image", "size": 10, group: 3 },
 
-    {"font": { "color": "white" }, "id": "SQL", "image": "icon/db.png", "label": "SQL", "shape": "image", "size": 10, group: 3 },
+    { "font": { "color": "white" }, "id": "SQL", "image": "icon/db.png", "label": "SQL", "shape": "image", "size": 10, group: 3 },
 
-    {"font": { "color": "white" }, "id": "Machine Learning", "label": "Machine Learning", "shape": "triangle", "size": 10, group: 3 },
+    { "font": { "color": "white" }, "id": "Machine Learning", "label": "Machine Learning", "shape": "triangle", "size": 10, group: 3 },
 
-    {"font": { "color": "white" }, "id": "Bash & Linux Systems", "label": "Bash & Linux Systems", "shape": "triangle", "size": 10, group: 3 },
+    { "font": { "color": "white" }, "id": "Bash & Linux Systems", "label": "Bash & Linux Systems", "shape": "triangle", "size": 10, group: 3 },
 
-    {"font": { "color": "white" }, "id": "Data Structures", "label": "Data Structures", "shape": "triangle", "size": 10, group: 3 },
+    { "font": { "color": "white" }, "id": "Data Structures", "label": "Data Structures", "shape": "triangle", "size": 10, group: 3 },
 
-    {"font": { "color": "white" }, "id": "Web Design", "label": "Web Design", "shape": "triangle", "size": 10, group: 3 },
+    { "font": { "color": "white" }, "id": "Web Design", "label": "Web Design", "shape": "triangle", "size": 10, group: 3 },
 
-    {"font": { "color": "white" }, "id": "Database Design", "label": "Database Design", "shape": "triangle", "size": 10, group: 3 },
+    { "font": { "color": "white" }, "id": "Database Design", "label": "Database Design", "shape": "triangle", "size": 10, group: 3 },
 
-    {"font": { "color": "white" }, "id": "Social Network Analysis", "label": "Social Network Analysis", "shape": "diamond", "size": 10, group: 1 },
+    { "font": { "color": "white" }, "id": "Social Network Analysis", "label": "Social Network Analysis", "shape": "diamond", "size": 10, group: 1 },
 
-    {"font": { "color": "white" }, "id": "Social Data Ethics", "label": "Social Data Ethics", "shape": "diamond", "size": 10, group: 1 },
+    { "font": { "color": "white" }, "id": "Social Data Ethics", "label": "Social Data Ethics", "shape": "diamond", "size": 10, group: 1 },
 
-    {"font": { "color": "white" }, "id": "Social Media Mining", "label": "Social Media Mining", "shape": "diamond", "size": 10, group: 1 },
+    { "font": { "color": "white" }, "id": "Social Media Mining", "label": "Social Media Mining", "shape": "diamond", "size": 10, group: 1 },
 
-    {"font": { "color": "white" }, "id": "Network Visualization", "label": "Network Visualization", "shape": "diamond", "size": 10, group: 1 },
+    { "font": { "color": "white" }, "id": "Network Visualization", "label": "Network Visualization", "shape": "diamond", "size": 10, group: 1 },
 
-    {"font": { "color": "white" }, "id": "Network Modeling", "label": "Network Modeling", "shape": "diamond", "size": 10, group: 1 },
+    { "font": { "color": "white" }, "id": "Network Modeling", "label": "Network Modeling", "shape": "diamond", "size": 10, group: 1 },
 
-    {"font": { "color": "white" }, "id": "Minor in Mathematics", "label": "Minor in Mathematics", "shape": "dot", "size": 10, group: 2 },
+    { "font": { "color": "white" }, "id": "Minor in Mathematics", "label": "Minor in Mathematics", "shape": "dot", "size": 10, group: 2 },
 
-    {"font": { "color": "white" }, "id": "B.Sc. Physics", "label": "B.Sc. Physics", "shape": "dot", "size": 10, group: 2 },
+    { "font": { "color": "white" }, "id": "B.Sc. Physics", "label": "B.Sc. Physics", "shape": "dot", "size": 10, group: 2 },
 
-    {"font": { "color": "white" }, "id": "M.Sc. Info. Mgmt Data Science ", "label": "M.Sc. Info. Mgmt Data Science ", "shape": "dot", "size": 10, group: 2 },
+    { "font": { "color": "white" }, "id": "M.Sc. Info. Mgmt Data Science ", "label": "M.Sc. Info. Mgmt Data Science ", "shape": "dot", "size": 10, group: 2 },
 
-]);
+    ]);
 
-    edges = new vis.DataSet([{ "from": "My Skills", "to": "Computer Science", width: 7}, 
-    { "from": "My Skills", "to": "Mathematics", width: 7}, 
-    { "from": "My Skills", "to": "Social Sciences", width: 7}, 
-    { "from": "Computer Science", "to": "Machine Learning"}, 
-    { "from": "Computer Science", "to": "Bash & Linux Systems"}, 
-    { "from": "Computer Science", "to": "Data Structures"}, 
-    { "from": "Machine Learning", "to": "Python"}, 
-    { "from": "Machine Learning", "to": "R"}, 
-    { "from": "Machine Learning", "to": "SQL"}, 
-    { "from": "Mathematics", "to": "B.Sc. Physics"}, 
-    { "from": "Mathematics", "to": "M.Sc. Info. Mgmt Data Science "}, 
-    { "from": "Mathematics", "to": "Minor in Mathematics"}, 
-    { "from": "Social Sciences", "to": "Social Data Ethics"}, 
-    { "from": "Social Sciences", "to": "Social Network Analysis"}, 
-    { "from": "Social Sciences", "to": "Social Media Mining"}, 
-    { "from": "Social Network Analysis", "to": "Network Modeling"}, 
-    { "from": "Social Network Analysis", "to": "Network Visualization"}, 
-    { "from": "Computer Science", "to": "Web Design"}, 
-    { "from": "Web Design", "to": "Database Design", }]);
+    edges = new vis.DataSet([
+        { "from": "My Skills", "to": "Computer Science", width: 7 },
+        { "from": "My Skills", "to": "Mathematics", width: 7 },
+        { "from": "My Skills", "to": "Social Sciences", width: 7 },
+        { "from": "Computer Science", "to": "Machine Learning" },
+        { "from": "Computer Science", "to": "Bash & Linux Systems" },
+        { "from": "Computer Science", "to": "Data Structures" },
+        { "from": "Machine Learning", "to": "Python" },
+        { "from": "Machine Learning", "to": "R" },
+        { "from": "Machine Learning", "to": "SQL" },
+        { "from": "Mathematics", "to": "B.Sc. Physics" },
+        { "from": "Mathematics", "to": "M.Sc. Info. Mgmt Data Science " },
+        { "from": "Mathematics", "to": "Minor in Mathematics" },
+        { "from": "Social Sciences", "to": "Social Data Ethics" },
+        { "from": "Social Sciences", "to": "Social Network Analysis" },
+        { "from": "Social Sciences", "to": "Social Media Mining" },
+        { "from": "Social Network Analysis", "to": "Network Modeling" },
+        { "from": "Social Network Analysis", "to": "Network Visualization" },
+        { "from": "Computer Science", "to": "Web Design" },
+        { "from": "Web Design", "to": "Database Design", }]);
 
     // adding nodes and edges to the graph
     data = { nodes: nodes, edges: edges };
@@ -88,8 +89,8 @@ function drawGraph() {
         "configure": {
             "enabled": false
         },
-        nodes:{
-            shadow:true
+        nodes: {
+            shadow: true
         },
         "edges": {
             arrows: {
@@ -98,7 +99,7 @@ function drawGraph() {
                     scaleFactor: 0.5
                 }
             },
-            shadow:true,
+            shadow: true,
             width: 2,
             "color": {
                 "inherit": true
@@ -135,16 +136,14 @@ function drawGraph() {
                 "iterations": 10000,
                 "onlyDynamicEdges": false,
                 "updateInterval": 50
-            }, 
+            },
         },
-        layout:{randomSeed:2}
+        layout: { randomSeed: 2 }
     };
 
 
     network = new vis.Network(container, data, options);
 
-
-  
 
 
     // get a JSON object
@@ -186,6 +185,20 @@ function updateNode(node, hidden) {
 
 network.on("hoverNode", function (params) {
     data.nodes.update({ id: "My Skills", "image": "../img/blue.png" });
+    var edges = data.edges.get({
+        filter: function (item) {
+            return item.from === params.node;
+        }
+    });
+
+    var list = [];
+    for (var n in edges) {
+        list.push({ edge: edges[n].id, trafficSize: 3});
+    }
+
+    if (list.length !== 0) {
+        network.animateTraffic(list);
+    }
 
 });
 
@@ -239,7 +252,7 @@ elementToFindDigitsIn.innerHTML =
         .replace("Social", "<span>Social</span>")
         .replace("Ethic", "<span>Ethic</span>")
         .replace("Social Media", "<span>Social Media</span>")
-        
+
 
 
 //Get the canvas HTML element
